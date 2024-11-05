6dab0788cf8de7cdab5149cac2d0940fee4229c1
EM.Nallar <miguel95.mn@gmail.com>
2024-11-05 08:14:20 -0300

    Routeando e eliminando funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,15 @@ import { Component } from '@angular/core';
 import { ListadoPeliculasComponent } from "./peliculas/listado-peliculas/listado-peliculas.component";
 import { MenuComponent } from "./compartidos/componentes/menu/menu.component";
 import { RatingComponent } from "./compartidos/componentes/rating/rating.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ListadoPeliculasComponent, MenuComponent, RatingComponent],
+  imports: [ListadoPeliculasComponent, MenuComponent, RatingComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent{
-  peliculasEnCines!: any[];
-  peliculasProximosEstrenos!: any[];
-  clickBoton(){
-    alert('Me has clickeado');
-  }
 
-  procesarVoto(voto: number){
-    alert(`Calificación otorgada: ${voto}`);
-  }
 }
