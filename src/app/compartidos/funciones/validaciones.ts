2b315bc94512b9ce4559f7fe3cc580101adaf3d8
EM.Nallar <miguel95.mn@gmail.com>
2024-11-07 11:45:56 -0300

    Adicionando validaciones personalizadas

---
+++ src/app/compartidos/funciones/validaciones.ts
@@ -0,0 +1,38 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function primeraLetaMayuscula(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const valor = <string>control.value;
+
+        if(!valor) return null;
+        if(valor.length === 0) return null;
+        
+        const primeraLetra = valor[0];
+
+        if(primeraLetra !== primeraLetra.toUpperCase()){
+            return {
+                primeraLetaMayuscula: {
+                    mensaje: 'Primera letra debe ser mayúscula'
+                }
+            }
+        }
+        return null;
+    }
+}
+
+export function noInicioEspacio(): ValidatorFn{
+    return (control: AbstractControl): ValidationErrors | null => {
+
+        const esSoloEspacio = control.value as string;
+
+        const comienzaConEspacio = esSoloEspacio.startsWith(' ');
+
+        if(comienzaConEspacio){
+        return {soloEspacios: {
+            mensaje: 'No es posible iniciar con espacio'       
+            }
+        };
+    }
+    return null;
+    };
+}
--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+import { noInicioEspacio, primeraLetaMayuscula } from '../../compartidos/funciones/validaciones';
 
 @Component({
   selector: 'app-crear-generos',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, MatFormFieldModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css'
 })
@@ -13,7 +17,31 @@ export class CrearGenerosComponent {
 
   router = inject(Router);
 
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    nombre: ['', {validators: [Validators.required, primeraLetaMayuscula(), noInicioEspacio()]}]
+  })
+
+  obtenerErrorCampoNombre(): string{
+    let nombre = this.form.controls.nombre;
+    if(nombre.hasError('required')){
+      return 'El campo nombre es requerido';
+    }
+
+    if(nombre.hasError('primeraLetaMayuscula')){
+      return nombre.getError('primeraLetaMayuscula').mensaje;
+    }
+
+    if(nombre.hasError('soloEspacios')){
+      return nombre.getError('soloEspacios').mensaje
+    }
+
+    return '';
+  }
+
   guardarCambios(){
-    this.router.navigate(['./generos']);
+    // this.router.navigate(['./generos']);
+    console.log(this.form.value);
   }
 }
