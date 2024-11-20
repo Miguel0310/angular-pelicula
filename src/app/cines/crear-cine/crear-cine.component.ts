bcb6fadf59357d164fcb5b273c968528eca9894f
EM.Nallar <miguel95.mn@gmail.com>
2024-11-20 09:15:11 -0300

    Formulario de Cine

--- src/app/cines/crear-cine/crear-cine.component.ts
+++ src/app/cines/crear-cine/crear-cine.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
+import { CineCreacionDTO } from '../cines';
 
 @Component({
   selector: 'app-crear-cine',
   standalone: true,
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './crear-cine.component.html',
   styleUrl: './crear-cine.component.css'
 })
 export class CrearCineComponent {
 
+  guardarCambios(cine: CineCreacionDTO){
+    console.log('Creando cine ',cine);
+  }
 }
--- src/app/cines/editar-cine/editar-cine.component.ts
+++ src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { CineCreacionDTO, CineDTO } from '../cines';
+import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
 
 @Component({
   selector: 'app-editar-cine',
   standalone: true,
-  imports: [],
+  imports: [FormularioCinesComponent],
   templateUrl: './editar-cine.component.html',
   styleUrl: './editar-cine.component.css'
 })
@@ -11,4 +13,10 @@ export class EditarCineComponent {
 
   @Input({transform: numberAttribute})
   id!: number;
+
+  cine: CineDTO = {id: 1, nombre: 'Acropolis'}
+
+  guardarCambios(cine: CineCreacionDTO){
+    console.log('Editar cine', cine);
+  }
 }
